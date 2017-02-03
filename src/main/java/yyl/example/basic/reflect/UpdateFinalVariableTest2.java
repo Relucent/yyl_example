c0e67249e42f082c4ce7cade0e595c945e4a33bf
relucent <relucent@163.com>
2017-02-03 17:51:30 +0800

    +

---
+++ src/main/java/yyl/example/basic/reflect/UpdateFinalVariableTest2.java
@@ -0,0 +1,20 @@
+package yyl.example.basic.reflect;
+
+import java.lang.reflect.Field;
+
+/**
+ * 通过反射修改常量池中的String(测试JDK1.7)<br>
+ * 这只是个测试，可能会引起不可预测的后果，应用中应避免这种用法<br>
+ */
+public class UpdateFinalVariableTest2 {
+
+	public static void main(String[] args) throws Exception {
+
+		Field field = String.class.getDeclaredField("value");
+		field.setAccessible(true);
+		field.set("hello", "hello world".toCharArray());
+
+		System.out.println("hello");//此处会打印 hello world (常量池中的字符串被修改了)
+	}
+
+}
