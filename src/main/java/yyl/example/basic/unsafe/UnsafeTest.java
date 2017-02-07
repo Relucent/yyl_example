3f1bf91abb224699302856ca19def96b6d90fc60
relucent <relucent@163.com>
2017-02-07 15:48:46 +0800

    +

--- src/main/java/yyl/example/basic/unsafe/UnsafeTest.java
+++ src/main/java/yyl/example/basic/unsafe/UnsafeTest.java
@@ -2,6 +2,8 @@ package yyl.example.basic.unsafe;
 
 import java.lang.reflect.Field;
 
+import io.netty.channel.Channel.Unsafe;
+
 /**
  * Unsafe <br>
  * 测试JDK1.7
@@ -9,31 +11,37 @@ import java.lang.reflect.Field;
 @SuppressWarnings("restriction")
 public class UnsafeTest {
 
-	public static void main(String[] args) throws Exception {
+	private static final sun.misc.Unsafe UNSAFE;
+	//通过反射去获取Unsafe的对象
+	static {
+		sun.misc.Unsafe unsafe = null;
+		try {
+			//通过反射获取rt.jar下的Unsafe类  
+			Field field = Unsafe.class.getDeclaredField("theUnsafe");
+			field.setAccessible(true);
+			unsafe = (sun.misc.Unsafe) field.get(null);
+		} catch (Exception e) {
+			System.err.println("Get Unsafe instance occur error" + e);
+		}
+		UNSAFE = unsafe;
+	}
 
-		sun.misc.Unsafe unsafe = getUnsafe();
+	public static void main(String[] args) throws Exception {
 
 		//获取属性偏移量，可以通过这个偏移量
 		Class<T> clazz = T.class;
 		Field field = clazz.getField("value");
-		long fieldOffset = unsafe.objectFieldOffset(field);
+		long fieldOffset = UNSAFE.objectFieldOffset(field);
 		System.out.println(field.getName() + ":" + fieldOffset);
 
 		T o = new T();
 
 		//更改字段的值 如果原始值是2 改为10
-		System.out.println(unsafe.compareAndSwapInt(o, fieldOffset, 1, 10));
+		System.out.println(UNSAFE.compareAndSwapInt(o, fieldOffset, 1, 10));
 
 		//更改字段的值 如果原始值是1 改为10
-		System.out.println(unsafe.compareAndSwapInt(o, fieldOffset, 0, 10));
+		System.out.println(UNSAFE.compareAndSwapInt(o, fieldOffset, 0, 10));
 
-	}
-
-	static sun.misc.Unsafe getUnsafe() throws Exception {
-		Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
-		Field f = unsafeClass.getDeclaredField("theUnsafe");
-		f.setAccessible(true);
-		return (sun.misc.Unsafe) f.get(null);
 	}
 
 	static class T {
