9e20aa419c876a06fdc8b3c656fc6a5687ad0d96
relucent <relucent@163.com>
2017-03-24 20:47:14 +0800

    +

--- src/main/java/yyl/example/demo/spring/configuration/ConfigurationBean.java
+++ src/main/java/yyl/example/demo/spring/configuration/ConfigurationBean.java
@@ -1,32 +1,37 @@
 package yyl.example.demo.spring.configuration;
 
-import java.lang.reflect.Method;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class ConfigurationBean {
 
-	@Bean(name = "myBean")
-	public Object myBean() {
-		System.out.println("create-bean1");
+	@Bean(name = "singletonBean")
+	/* @Scope("singleton") */
+	public Object singletonBean() {
+		System.out.println("create-singleton");
 		return new Object();
 	}
 
-	@Bean(name = "myBeanTest")
-	public Object myBeanTest() throws Exception {
+	@Bean(name = "multitonBean")
+	@Scope("prototype")
+	public Object multitonBean() {
+		System.out.println("create-multiton");
+		return new Object();
+	}
+
+	@Bean(name = "afterCreateBeanTest")
+	public Object afterCreateBeanTest() throws Exception {
 
 		//执行多次，返回的对象是相同的
+		System.out.println("singletonBean() == singletonBean() -> " + (singletonBean() == singletonBean()));
 
-		Object bean1 = myBean();
-		Object bean2 = myBean();
-
-		System.out.println("myBean() == myBean() -> " + (bean1 == bean2));
+		//执行多次，返回的对象是不同的
+		System.out.println("multitonBean() == multitonBean() -> " + (multitonBean() == multitonBean()));
 
 		//因为这是一个代理方法
-		Method method = this.getClass().getMethod("myBean");
-		System.out.println(method);
+		System.out.println(this.getClass().getMethod("multitonBean"));
 
 		return new Object();
 	}
