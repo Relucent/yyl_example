c2cce2eb4cb5e7d4cfe15e74796c745c9527146e
relucent <relucent@163.com>
2017-04-11 10:33:29 +0800

    +

---
+++ src/main/java/yyl/example/demo/redis/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * REmote DIctionary Server(Redis) 是一个由Salvatore Sanfilippo写的key-value存储系统。<br>
+ * Redis是一个开源的使用ANSI C语言编写、遵守BSD协议、支持网络、可基于内存亦可持久化的日志型、Key-Value数据库，并提供多种语言的API。<br>
+ * 它通常被称为数据结构服务器，因为值（value）可以是 字符串(String), 哈希(Map), 列表(list), 集合(sets) 和 有序集合(sorted sets)等类型。<br>
+ * 官网地址 [http://redis.io]<br>
+ * 
+ * Redis官方是不支持windows的，但是 Microsoft Open Tech group 在 GitHub上开发了一个Win64的版本<br>
+ * 项目地址： [https://github.com/MSOpenTech/redis]<br>
+ */
+package yyl.example.demo.redis;
