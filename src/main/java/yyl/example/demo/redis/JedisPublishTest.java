7f70ad4d4f706a05ee77a52b79053c64efb026d9
relucent <relucent@163.com>
2017-04-17 19:16:22 +0800

    +

---
+++ src/main/java/yyl/example/demo/redis/JedisPublishTest.java
@@ -0,0 +1,60 @@
+package yyl.example.demo.redis;
+
+import java.text.ParseException;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
+
+/**
+ * Jedis 发布订阅
+ */
+public class JedisPublishTest {
+
+	private static final String CHANNEL_KEY = "my-test-channel";
+
+	public static void main(String[] args) throws ParseException {
+		new Thread(new Subcriber("S1")).start();
+		new Thread(new Subcriber("S2")).start();
+		new Thread(new Subcriber("S3")).start();
+		new Thread(new Publisher()).start();
+	}
+
+	private static class Publisher implements Runnable {
+		@Override
+		public void run() {
+			long sequence = 0;
+			try (Jedis jedis = new Jedis("localhost", 6379)) {
+				while (true) {
+					jedis.publish(CHANNEL_KEY, "MESSAGE#" + (sequence++));
+					try {
+						Thread.sleep(2 * 1000);
+					} catch (InterruptedException e) {
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	private static class Subcriber implements Runnable {
+
+		final String name;
+
+		public Subcriber(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public void run() {
+			try (Jedis jedis = new Jedis("localhost", 6379)) {
+				jedis.subscribe(new JedisPubSub() {
+					@Override
+					public void onMessage(String channel, String message) {
+						System.out.println(name + " <- " + message);
+					}
+				}, CHANNEL_KEY);
+			}
+		}
+	}
+
+}
