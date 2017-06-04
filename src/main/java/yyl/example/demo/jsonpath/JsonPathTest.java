d1d961de744a39324dcdc928549ffe810cb6fa22
relucent <relucent@163.com>
2017-06-04 10:00:08 +0800

    @

--- src/main/java/yyl/example/demo/jsonpath/JsonPathTest.java
+++ src/main/java/yyl/example/demo/jsonpath/JsonPathTest.java
@@ -5,7 +5,7 @@ import com.jayway.jsonpath.JsonPath;
 import yyl.example.basic.util.Streams;
 
 /**
- * JsonPath是一个读取JSON的工具类，使用了一种类似xpath的表达式语言。<br>
+ * JsonPath是一个读取JSON的工具类，使用路径表达式来选取JSON中的节点或节点集。<br>
  * 官网地址: https://github.com/jayway/JsonPath
  */
 public class JsonPathTest {
@@ -44,12 +44,12 @@ public class JsonPathTest {
 //	|语法					|描述					|
 //	|:-----				|:----- 			|
 //	|$					|根节点				|
-//	|@					|当前节点				|
-//	|*					|所有节点				|
-//	|..					|选择所有符合条件的节点	|
+//	|@					|当前节点正在由筛选器谓词处理	|
+//	|*					|通配符，匹配任何属性名称		|
+//	|..					|选择所有符合条件的节点		|
 //	|.or[]				|子节点				|
-//	|[]					|迭代器标示，如数组下标	|
-//	|[,]				|支持迭代器中做多选		|
+//	|[]					|迭代器标示，如数组下标		|
+//	|[,]				|支持迭代器中做多选			|
 //	|[start:end:step]	|数组切片运算符			|
 //	|?()				|支持过滤操作			|
 //	|()					|支持表达式计算			|
