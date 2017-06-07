ae4265d4145e29ba4ddb60afd5039add7a90ba93
relucent <relucent@163.com>
2017-06-07 22:11:25 +0800

    !

---
+++ src/main/java/yyl/example/basic/thread/NoThreadSafe.java
@@ -0,0 +1,26 @@
+package yyl.example.basic.thread;
+
+/**
+ * 线程不安全测试
+ */
+public class NoThreadSafe {
+
+	private static boolean flag = true;
+
+	public static void main(String[] args) {
+		Thread daemon = new Thread() {
+			public void run() {
+				while (true) {
+					flag = !flag;
+				}
+			};
+		};
+		daemon.setDaemon(true);
+		daemon.start();
+
+		while (flag == true || flag == false) {
+		}
+		System.out.println(flag);
+		System.out.println("Exit!");
+	}
+}
