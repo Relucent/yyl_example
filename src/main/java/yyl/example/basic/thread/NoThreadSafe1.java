da082ea7cc26ec356e5878155ecad8cc59a7369b
relucent <relucent@163.com>
2017-06-08 07:54:00 +0800

    #

--- src/main/java/yyl/example/basic/thread/NoThreadSafe1.java
+++ src/main/java/yyl/example/basic/thread/NoThreadSafe1.java
@@ -18,6 +18,8 @@ public class NoThreadSafe1 {
 		daemon.setDaemon(true);
 		daemon.start();
 
+		// (flag == true || flag == false) 不是原子操作
+		// 这个语句中，其实是取了两次flag，分别判断其取值
 		while (flag == true || flag == false) {
 		}
 		System.out.println(flag);
