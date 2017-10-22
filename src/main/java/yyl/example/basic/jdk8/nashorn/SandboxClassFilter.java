546b87a77a823b82d056c512b0d1510143e64559
relucent <relucent@163.com>
2017-10-22 12:25:38 +0800

    #

---
+++ src/main/java/yyl/example/basic/jdk8/nashorn/SandboxClassFilter.java
@@ -0,0 +1,19 @@
+package yyl.example.basic.jdk8.nashorn;
+
+import java.util.Set;
+
+import jdk.nashorn.api.scripting.ClassFilter;
+
+@SuppressWarnings("restriction")
+public class SandboxClassFilter implements ClassFilter {
+	private final Set<String> allowed;
+
+	@Override
+	public boolean exposeToScripts(final String className) {
+		return this.allowed.contains(className);
+	}
+
+	public SandboxClassFilter(final Set<String> allowed) {
+		this.allowed = allowed;
+	}
+}
