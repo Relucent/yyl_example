dcf257d3e31a63c0f3d337e9d67ee362d4b1492f
yyl <relucent@163.com>
2018-12-01 18:31:38 +0800

    #

--- src/main/java/yyl/example/basic/number/BigDecimalTest.java
+++ src/main/java/yyl/example/basic/number/BigDecimalTest.java
@@ -16,5 +16,13 @@ public class BigDecimalTest {
 
 		System.out.println(value1);
 		System.out.println(value2);
+		
+		BigDecimal value3 = new BigDecimal("1E10");
+		//工程计数法(和科学计数法类似，但是幂为 10^3的倍数)
+		System.out.println(value3.toEngineeringString());
+		//科学计数法
+        System.out.println(value3.toString());
+		//不带指数字段
+		System.out.println(value3.toPlainString());
 	}
 }
