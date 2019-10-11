5546a94536b289c061322bd8573628d718b8cef0
yyl <relucent@163.com>
2019-10-11 19:06:23 +0800

    >

--- src/main/java/yyl/example/basic/concurrent/AbstractQueuedSynchronizerTest.java
+++ src/main/java/yyl/example/basic/concurrent/AbstractQueuedSynchronizerTest.java
@@ -42,17 +42,32 @@ public class AbstractQueuedSynchronizerTest {
         @Override
         protected boolean tryAcquire(int acquires) {
             final Thread current = Thread.currentThread();
+
+            // 获得变量state
             int c = getState();
+
+            // 判断锁是否被占用，0代表未被占用
             if (c == 0) {
+                // 断队列中是否有其他线程在等待
+                if (hasQueuedPredecessors()) {
+                    return false;
+                }
+                // 设置锁的状态为1 (被占用)
                 if (compareAndSetState(0, acquires)) {
+                    // 设置占用排它锁的线程是当前线程
                     setExclusiveOwnerThread(current);
                     return true;
                 }
-            } else if (current == getExclusiveOwnerThread()) {
+            }
+            // 当前锁已被占用，但是占用锁的是当前线程本身
+            else if (current == getExclusiveOwnerThread()) {
+                // 当已经获取锁的线程每多获取一次锁，state就执行加1操作 (重入的实现)
                 int nextc = c + acquires;
+                // 超过最大锁定计数(Integer.MAX_VALUE)，溢出了
                 if (nextc < 0) {// overflow
                     throw new Error("Maximum lock count exceeded");
                 }
+                // 当占用锁的是当前线程本身，所以不需要CAS，直接设置状态值即可
                 setState(nextc);
                 return true;
             }
@@ -62,16 +77,26 @@ public class AbstractQueuedSynchronizerTest {
         // 试图设置状态来反映独占模式下的一个释放
         @Override
         protected final boolean tryRelease(int releases) {
+            // state就执行减1操作 (重入的实现)
             int c = getState() - releases;
+            // 如果占用锁的线程不是当前线程，抛出异常(这种情况一般是代码写的有问题)
             if (Thread.currentThread() != getExclusiveOwnerThread()) {
                 throw new IllegalMonitorStateException();
             }
+
+            // 此对象现在处于完全释放状态(其他等待的线程都可以试图获得锁)
             boolean free = false;
+
+            // 0代表未被占用
             if (c == 0) {
+                // 完全释放状态为TRUE
                 free = true;
+                // 设置占用排它锁的线程是NULL
                 setExclusiveOwnerThread(null);
             }
             setState(c);
+
+            // 返回完全释放状态状态
             return free;
         }
     }
