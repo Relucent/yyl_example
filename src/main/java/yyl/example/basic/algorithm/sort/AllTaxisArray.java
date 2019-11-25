a8913aad655fd1c733d134df0a2fa34609d19978
yyl <relucent@163.com>
2019-11-25 18:39:42 +0800

    #

--- src/main/java/yyl/example/basic/algorithm/sort/AllTaxisArray.java
+++ src/main/java/yyl/example/basic/algorithm/sort/AllTaxisArray.java
@@ -1,40 +1,35 @@
 package yyl.example.basic.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * 全排序
  * @author yyl
  */
 public class AllTaxisArray {
 
-	public static void main(String[] s) {
-		long l = System.currentTimeMillis();
-		Object[] src = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-		Object[] arrays = new Object[src.length];
-		System.arraycopy(src, 0, arrays, 0, src.length);
-		recursionAllTaxis(arrays, 0);
-		System.out.println(System.currentTimeMillis() - l);
-	}
+    public static void main(String[] s) {
+        Object[] src = {"0", "1", "2",};
+        Object[] arrays = new Object[src.length];
+        System.arraycopy(src, 0, arrays, 0, src.length);
+        recursionAllTaxis(arrays, 0);
+    }
 
-	static public void recursionAllTaxis(Object[] arrays, int indexLevel) {
-		if (indexLevel == arrays.length - 1) {
-			// for(Object o:arrays){
-			// System.out.print(o);
-			// System.out.print(';');
-			// }
-			// System.out.println();
-		}
-		else {
-			for (int i = indexLevel; i < arrays.length; i++) {
-				swap(arrays, indexLevel, i);
-				recursionAllTaxis(arrays, indexLevel + 1);
-				swap(arrays, indexLevel, i);
-			}
-		}
-	}
+    static public void recursionAllTaxis(Object[] arrays, int indexLevel) {
+        if (indexLevel == arrays.length - 1) {
+            System.out.println(Arrays.toString(arrays));
+        } else {
+            for (int i = indexLevel; i < arrays.length; i++) {
+                swap(arrays, indexLevel, i);
+                recursionAllTaxis(arrays, indexLevel + 1);
+                swap(arrays, indexLevel, i);
+            }
+        }
+    }
 
-	final static public void swap(Object[] arrays, int index0, int index1) {
-		Object obj = arrays[index0];
-		arrays[index0] = arrays[index1];
-		arrays[index1] = obj;
-	}
+    final static public void swap(Object[] arrays, int index0, int index1) {
+        Object obj = arrays[index0];
+        arrays[index0] = arrays[index1];
+        arrays[index1] = obj;
+    }
 }
--- src/main/java/yyl/example/basic/algorithm/sort/Quicksort.java
+++ src/main/java/yyl/example/basic/algorithm/sort/Quicksort.java
@@ -9,62 +9,62 @@ import java.util.Random;
  */
 public class Quicksort {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[] array = generate(10);
-		System.out.println(Arrays.toString(array));
+        int[] array = generate(10);
+        System.out.println(Arrays.toString(array));
 
-		sort(array, 0, array.length - 1);
-		System.out.println(Arrays.toString(array));
-	}
+        srot(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
 
-	private static int[] generate(int size) {
-		int[] array = new int[size];
-		for (int i = 0; i < size; i++) {
-			array[i] = i;
-		}
-		Random rnd = new Random();
-		for (int i = size - 1; i >= 0; i--) {
-			swap(array, i, rnd.nextInt(i + 1));
-		}
-		return array;
-	}
 
-	public static void sort(int[] array, int left, int right) {
-		if (left < right) {
-			int i = left;
-			int j = right;
-			int temp = array[left];//基数
-			// i < j
-			while (i != j) {
-				//顺序很重要，要先从右边开始找 
-				while (i < j && temp <= array[j]) {
-					j--;
-				}
-				//再从左边开始找 
-				while (i < j && array[i] <= temp) {
-					i++;
-				}
-				//交换两个数在数组中的位置 
-				if (i < j) {
-					swap(array, i, j);
-					System.out.println(Arrays.toString(array) + " |*" + i + "<=>*" + j);
-				}
-			}
-			//将基准数归位
-			swap(array, i, left);
+    public static void srot(int[] array, int left, int right) {
+        if (left < right) {
+            int i = left;
+            int j = right;
+            int x = array[i];// 基数
+            while (i < j) {
+                // 基数是从左边取的，所以要先从右边开始查找
+                while (i < j && array[j] >= x) {
+                    j--;
+                }
+                // 再从左边开始找
+                while (i < j && array[i] <= x) {
+                    i++;
+                }
+                // 交换两个数在数组中的位置
+                if (i < j) {
+                    swap(array, i, j);
+                }
+            }
+            // 将 基数移动到位置(分割点的位置)
+            swap(array, left, i);
+            System.out.println("i=" + i);
 
-			System.out.println(Arrays.toString(array) + " |#" + i);
+            // 递归
+            srot(array, left, i - 1);
+            srot(array, i + 1, right);
+        }
+    }
 
-			sort(array, left, i - 1);
-			sort(array, i + 1, right);
-		}
-	}
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        System.out.println(Arrays.toString(array) + " |*" + i + "<=>*" + j);
+    }
 
-	private static void swap(int[] array, int i, int j) {
-		int temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-
+    // 用来生成测试数据的
+    private static int[] generate(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = i;
+        }
+        Random rnd = new Random();
+        for (int i = size - 1; i >= 0; i--) {
+            swap(array, i, rnd.nextInt(i + 1));
+        }
+        return array;
+    }
 }
