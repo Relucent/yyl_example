09cbede59e650a665573c44019afdc25695969fc
yyl <relucent@163.com>
2019-12-09 11:28:46 +0800

    #

--- src/main/java/yyl/example/basic/sound/PlayMidi.java
+++ src/main/java/yyl/example/basic/sound/PlayMidi.java
@@ -7,43 +7,53 @@ import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 
 /**
- * 播放MIDI音乐 (Window-XP,7,8)
+ * 播放MIDI音乐 (Window-XP,7,8,10)
  */
 public class PlayMidi {
-	private static Sequencer midi;
-	public static void play(URL url) {
-		try {
-			Sequence seq = MidiSystem.getSequence(url);
-			midi = MidiSystem.getSequencer();
-			midi.open();
-			midi.setSequence(seq);
 
-			if (!midi.isRunning())
-				midi.start();
+    public static void main(String[] args) {
 
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+        // 获得唱片地址
+        URL url = PlayMidi.class.getResource("doraemon.mid");
 
-	public static void stop() {
-		if (midi.isRunning())
-			midi.stop();
+        // 播放器
+        Sequencer sequencer = null;
+        try {
+            // 得到唱片 (指定的MIDI序列)
+            Sequence seq = MidiSystem.getSequence(url);
 
-		if (midi.isOpen())
-			midi.close();
-	}
+            // 得到播放器
+            sequencer = MidiSystem.getSequencer();
 
-	public static void main(String[] args) {
-		URL url = PlayMidi.class.getResource("doraemon.mid");
-		PlayMidi.play(url);
-		long time = midi.getMicrosecondLength() / 1000;
-		try {
-			Thread.sleep(time);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+            // 打开设备
+            sequencer.open();
 
-		PlayMidi.stop();
-	}
+            // 将唱片放入播放器
+            sequencer.setSequence(seq);
+
+            // 获得当前音乐的长度(单位微秒)
+            long time = sequencer.getMicrosecondLength() / 1000;
+
+            // 开始播放音乐
+            sequencer.start();
+
+            // 等待音乐播放结束
+            Thread.sleep(time);
+
+            // 是否正处于播放状态
+            if (sequencer.isRunning()) {
+                // 停止播放
+                sequencer.stop();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 如果设备处于开启状态
+            if (sequencer != null && sequencer.isOpen()) {
+                // 关闭设备(释放使用的系统资源)
+                sequencer.close();
+            }
+        }
+    }
 }
