988ff9b0cc56adc5737e9dabfe24a77e196d6b5b
yyl <relucent163.com>
2023-08-05 10:27:32 +0800

    +

--- src/main/java/yyl/example/exercise/awt/other/SoundBeep.java
+++ src/main/java/yyl/example/exercise/awt/other/SoundBeep.java
@@ -1,29 +1,44 @@
 package yyl.example.exercise.awt.other;
-import java.awt.Button;
-import java.awt.Dimension;
-import java.awt.Label;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class SoundBeep {
-	public static void main(String args[]) {
-		JFrame jFrame = new JFrame("单击按钮事件");
-		jFrame.setSize(new Dimension(200, 180));
-		JPanel jPanel = new JPanel();
-		jFrame.add(jPanel); // 将面板P添加到窗口f中
-		jPanel.add(new Label("按一下按钮可听到响声！", Label.CENTER));
-		Button button = new Button("按钮");
-		jPanel.add(button);
-		// 创建事件监听器对象
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Toolkit.getDefaultToolkit().beep();
-			}
-		}); // 注册事件监听器对象
-		jFrame.setVisible(true);
-	}
+    public static void main(String args[]) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("单击按钮事件");
+            Container container = frame.getContentPane();
+            container.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+            container.add(new JLabel("按一下按钮可听到响声！", JLabel.CENTER));
+            JButton button = new JButton("按钮");
+            container.add(button);
+
+            // 创建事件监听器对象
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    Toolkit.getDefaultToolkit().beep();
+                }
+            });
+
+            frame.setLocationRelativeTo(null);
+            frame.pack();
+            frame.setVisible(true);
+        });
+    }
 }
